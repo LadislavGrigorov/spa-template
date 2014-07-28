a65db7bd6bf228ae148f81593681f5a9119c9bd4
Ladislav Grigorov <ladislavgrigorov@gmail.com>
2014-07-28 15:02:28 +0300

    spa template

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+/*globals require, alert, console*/
+(function () {
+    'use strict';
+    require.config({
+        paths: {
+            jquery: 'libs/jquery-2.1.1',
+            mustache: 'libs/mustache',
+            q: 'libs/q',
+            sammy: 'libs/sammy'
+        }
+    });
+
+    require(['sammy', 'jquery'], function (Sammy, $) {
+        var app = Sammy('#main-content', function () {
+            this.get('#/', function () {
+				alert('Hi!');
+            });
+        });
+
+        $(function() {
+            app.run('#/');
+        });
+    });
+}());
